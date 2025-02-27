fda4212275ca557c57b7652b5b1290d0ffdc4e20
Hasan Hasan <Hjhasan@umich.edu>
2025-02-27 00:34:46 -0500

    Updated values of a, b, and c in quadraticsolver.java

---
+++ quadraticsolver.java
@@ -0,0 +1,12 @@
+public class QuadraticSolver {
+    public static void main(String[] args) {
+        // Define coefficients
+        int a = 6;  // Modified value
+        int b = 11; // Modified value
+        int c = 4;  // Modified value
+
+        // Print Hello World message
+        System.out.println("Hello, World!");
+        System.out.println("Values: a = " + a + ", b = " + b + ", c = " + c);
+    }
+}
